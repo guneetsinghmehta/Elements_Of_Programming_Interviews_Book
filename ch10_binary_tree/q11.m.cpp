942a2cb4c843cec505a17913743f688d2b1fcfea
Guneet Singh Mehta <mehta.guneet@gmail.com>
2017-09-11 01:32:07 +0100

    implement inOrder traversal with O(1) space and no recursion

--- ch10_binary_tree/q11.m.cpp
+++ ch10_binary_tree/q11.m.cpp
@@ -22,15 +22,54 @@ int main() {
 	root->right = new TreeNode(3);
 	root->right->parent = root;
 
+	root->left->right = new TreeNode(4);
+	root->left->right->parent = root->left;
+
 	inOrderTraversal(root);
 	return 1;
 }
 
 void inOrderTraversal(TreeNode* root){
 	using namespace std;
+	char direction = 'L'; // possible values being 'L' and 'R' and 'U'
+	while ( root != NULL ){
+		if(direction == 'L'){
+			if(root->left != NULL){
+				root = root->left;
+			}
+			else{
+				direction = 'R';	
+			}
+		}
+		else if (direction == 'R') {
+			cout << root->d_val << '\n';
+			if(root->right != NULL){
+				root = root->right;
+				direction = 'L';
+			}
+			else{
+				direction = 'U';
+			}
+		}
+		else if(direction == 'U'){
+			if(root != NULL && root->parent != NULL && root->parent->left == root){
+				root = root->parent;
+				direction = 'R';
+			}
+			else if(root != NULL && root->parent != NULL && root->parent->right == root){
+				root = root->parent;
+			}
+			else if(root != NULL && root->parent == NULL){
+				root = NULL;
+			}
+		}
+	}
+	
+	/*
 	if(root != NULL){
 		inOrderTraversal(root->left);
 		cout << root->d_val << '\n';
  		inOrderTraversal(root->right);
 	}
+	*/
 }
